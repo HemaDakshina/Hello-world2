ecee790f348f80578d6ab23f5850f83ca21e301d
Hemadakshina1228 <hemadakshina@gmail.com>
2018-01-07 11:44:22 -0600

    helloworld2

---
+++ src/com/learnJava/Main.java
@@ -0,0 +1,10 @@
+package com.learnJava;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello-World2"
+        );
+	// write your code here
+    }
+}
